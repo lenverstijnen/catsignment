922914b41ccdcb9774111478a69f71a02b65a36a
Len Verstijnen <lenverstijnen@me.com>
2021-03-24 22:07:59 +0100

    Add Characteristics to Card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,14 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  CatCharacteristicsService,
+  ICatCharacteristics,
+} from '../services/cat-characteristics.service';
 
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input('url') url: string | undefined;
+  catdetails: ICatCharacteristics;
 
-  constructor() {}
+  // By adding the characteristics here this component is not reusable. Wouldn't do this normaly
+  // but because of time reasons I leave it like this for now.
+  constructor(private service: CatCharacteristicsService) {
+    this.catdetails = service.getRandomCharacteristics();
+  }
 
-  ngOnInit(): void {}
+  handleContact(catname: string) {
+    alert(
+      `We are so sorry to tell you that you and ${catname} are not a match. Our matching algorithm of 136 rules didn't see any possible positive outcomes.`
+    );
+  }
 }
