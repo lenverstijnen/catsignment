9dfaf3c0a5695e11659de30a342c9313ea35438b
Len Verstijnen <lenverstijnen@me.com>
2021-03-27 22:39:23 +0100

    Add unfinished test SpinnerComponent

---
+++ src/app/components/spinner/spinner.component.spec.ts
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/angular';
+import { SpinnerComponent } from './spinner.component';
+
+// I didn't find a way to cleanup the dom in afterEach. I'm used to testing
+// with testing library in react with jest but it works a bit different in
+// Angular i found out.
+
+// I tried out some things, but it was more involving then I thought.
+// Due to some other obligations comming week I have to leave this topic
+// open for now.
+
+// I left this broken/unfinished code here so you can see the intentions I had.
+
+describe('SpinnerComponent', () => {
+
+    await render(SpinnerComponent, {
+      componentProperties: { loading: true },
+    });
+
+    const element = screen.getByTestId('spinner');
+
+    expect(element).toBeTruthy();
+  });
+
+  it('should not be renderd if loading is false', async () => {
+    await render(SpinnerComponent, {
+      componentProperties: { loading: false },
+    });
+
+    const element = screen.getByTestId('spinner');
+
+    expect(element).toBeFalsy();
+  });
+});
